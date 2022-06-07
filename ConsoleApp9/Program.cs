c3bce6439c8524f2396acefc539980cfe2d03fb1
saydi <saydi@ENGINEER>
2022-06-07 18:28:50 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите сумму покупки ");
+        double pok = double.Parse(Console.ReadLine());
+        double skid = 0.1;
+        if (pok > 1000)
+        {
+            skid = pok * skid;
+            pok = pok - skid;
+        }
+        Console.WriteLine("Сумма стоимости с учетом скидки " + pok);
+    }
+}
